defac908ebfe9c0e12431aafa6fefe8841a2468b
ilavriv <lavriv92@gmail.com>
2016-10-26 21:35:38 +0300

    Added pllug students

---
+++ format-stydents.py
@@ -0,0 +1,15 @@
+import sys
+import argparse
+import logging
+
+def main():
+    try:
+        input_file = sys.argv[1]
+    except IndexError:
+        return logging.error('Students file does not exists')
+    with open(input_file, 'r') as f:
+        file_data = f.read()
+        sys.stdout.write(',\n'.join(file_data.split('\n')))
+
+if __name__ == '__main__':
+    main()
